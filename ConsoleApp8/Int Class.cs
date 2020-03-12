e3e7c1b144d1b30df38626d6ab617751561b2c66
Jose <you@example.com>
2020-03-11 19:10:08 -0500

    lesson

---
+++ ConsoleApp8/Int Class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8
+{
+    class Int_Class
+    {
+        public int Num1;
+        public int Num2;
+        private int Num3;
+
+        public Int_Class(int aNum, int bNum, int cNum)
+        {
+            Num1 = aNum;
+            Num2 = bNum;
+            NUM3 = cNum; 
+        }
+        public int NUM3
+        {
+            get { return Num3; }
+            set
+            {
+                if(value <= 10 && value >= 0)
+                {
+                    Num3 = value; 
+                }
+                else
+                {
+                    Console.WriteLine("type in a number between 0-10");     
+                }
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+//write a program in c# to find the sum of all elements of the array 
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] Sum = new int[5];
+            //int sum1 = Sum[0] = 2;
+            //int sum2 = Sum[1] = 4;
+            //int sum3 = Sum[2] = 6;
+            //int sum4 = Sum[3] = 8;
+            //int sum5 = Sum[4] = 10;
+
+            //int total = sum1 + sum2 + sum3 + sum4 + sum5;
+            //Console.WriteLine(total); 
+
+            //int i = Sum.Length-1;
+            //if(i>=0)
+            //{
+
+            //}
+
+            Int_Class SumOSum = new Int_Class(10, 20, 30);
+            Console.WriteLine(SumOSum.NUM3); 
+                
+
+        }
+    }
+}
